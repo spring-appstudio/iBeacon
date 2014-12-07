7c5ea233710660f0be240cddf8b7a9a5d4951e9d
Ryan C. Spring <github@spring-appstudio.de>
2014-12-07 15:24:13 -0500

    Initial Commit

---
+++ iBeaconTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconTest
+//
+//  Created by Ryan C. Spring on 12/7/14.
+//  Copyright (c) 2014 Spring-Appstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
